29043906746f5d6b5800152471ea287e7e08dec9
nikhil-mangla <e22cseu0288@bennett.edu.in>
2025-03-21 08:03:00 +0530

    updated routing

--- backend/rides/routing.py
+++ backend/rides/routing.py
@@ -3,5 +3,4 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/rides/$', consumers.RideConsumer.as_asgi()),
-    re_path(r'ws/rides/(?P<ride_id>\d+)/$', consumers.RideConsumer.as_asgi()),
 ]
